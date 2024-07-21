2d0e0791430200de427e2f1e4aa1b9d837343090
jonathandang <danghjon@gmail.com>
2024-07-21 14:57:39 -0500

    updatiwng file path

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-//Import the THREE.js library
+// Import the THREE.js library
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 // To allow for the camera to move around the scene
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
