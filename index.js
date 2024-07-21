1df4344449cf781a25946c10feb73a706d80633e
jonathandang <danghjon@gmail.com>
2024-07-21 14:30:10 -0500

    updatiwng file path

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const fs = require('fs'); // Import the 'fs' module
 const path = require('path'); // Import the 'path' module
+
 const app = express();
 
 // Use environment variable for port, with fallback to 3000
 const port = process.env.PORT || 3000;
 
 // Serve static files from the root directory
-app.use(express.static(path.join(__dirname, './')));
+app.use(express.static(path.join(__dirname, '.')));
 
 // Define a route to serve the index.html file
-app.get(['/home', '/', '/main'], (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
